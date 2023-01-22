6840a679199369e310ea45d6e3175320fc1205f5
stanislove <stashevvsky@gmail.com>
2023-01-22 10:06:54 +0000

    source cosmos profile

--- cosmos/install_node.sh
+++ cosmos/install_node.sh
@@ -3,7 +3,9 @@ source $HOME/.bash_profile
 bash <(curl -s https://raw.githubusercontent.com/stanisloe/bash-scripts/master/cosmos/init_node.sh)
 bash <(curl -s https://raw.githubusercontent.com/stanisloe/bash-scripts/master/cosmos/setup_ports.sh)
 bash <(curl -s https://raw.githubusercontent.com/stanisloe/bash-scripts/master/cosmos/add_wallet.sh)
+source $HOME/$COSMOS_PROFILE_FILE_NAME
 bash <(curl -s https://raw.githubusercontent.com/stanisloe/bash-scripts/master/cosmos/install_cosmovisor.sh)
+source $HOME/$COSMOS_PROFILE_FILE_NAME
 bash <(curl -s https://raw.githubusercontent.com/stanisloe/bash-scripts/master/cosmos/install_commands.sh)
 
 source $HOME/$COSMOS_PROFILE_FILE_NAME
