f3807bef52475f63e5dbf35df8c93c0965cfeb52
stanislove <stashevvsky@gmail.com>
2023-02-11 11:15:30 +0000

    add more debug info

--- cosmos/install_cosmovisor_2.sh
+++ cosmos/install_cosmovisor_2.sh
@@ -12,11 +12,15 @@ if [ -f /etc/systemd/system/$COSMOS_SERVICE_NAME ]; then
     exit 1
 fi
 
+
 go install github.com/cosmos/cosmos-sdk/cosmovisor/cmd/cosmovisor@latest
+
+echo "initializing cosmovisor"
 export DAEMON_NAME=$COSMOS_BINARY
 export DAEMON_HOME=$COSMOS_NODE_PATH
 cosmovisor init $(which $COSMOS_BINARY)
 
+echo "createing service file"
 sudo tee /etc/systemd/system/$COSMOS_SERVICE_NAME > /dev/null <<EOF
 [Service]
 User=$USER
